f815dddff96c5a10c2f1e045b2dfcdc34f156766
joeban <joeban@twbeex.com>
2023-12-18 14:13:56 +0800

    feat: 5-69-to-71 Creating HTML Pages

--- routes/404.js
+++ routes/404.js
@@ -1,6 +1,8 @@
+const path = require("path");
+
 const express = require("express");
 const router = express.Router();
 router.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "../", "views", "404.html"));
 });
 module.exports = router;
