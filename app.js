ddb70732b99f6b4e6208c8e5b6bbac21ad75f0ed
joeban <joeban@twbeex.com>
2023-12-21 15:58:21 +0800

    feat: 6-79 Sharing Data Across Requests & Users

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const adminRoutes = require("./routes/admin");
+const adminModule = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 const errorRoute = require("./routes/404");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminModule.routes);
 app.use(shopRoute);
 app.use(errorRoute);
 
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,10 @@ const rootDir = require("../utils/path");
 
 const express = require("express");
 const router = express.Router();
+const adminModule = require("./admin");
+
 router.get("/", (req, res, next) => {
+  console.log("adminModule.products", adminModule.products);
   res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 module.exports = router;
